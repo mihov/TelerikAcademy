e656adf8f6b68bc1b31b993f8c566c7556f7e125
Tancho Mihov <tancho.mihov@gmail.com>
2014-03-02 01:01:34 +0200

    BitAray64

--- OOP/06 Common Type System/ProblemBitArray64/BitArray64.cs
+++ OOP/06 Common Type System/ProblemBitArray64/BitArray64.cs
@@ -7,22 +7,104 @@
     using System.Threading.Tasks;
     public class BitArray64 : IEnumerable<int>
     {
+        private ulong number;
+
+        public BitArray64(ulong number = 0)
+        {
+            this.number = number;
+        }
+
         public IEnumerator<int> GetEnumerator()
         {
-            throw new NotImplementedException();
+            int[] bits = this.GetBits();
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                yield return bits[i];
+            }
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
         }
 
         public int this[int index]
         {
             get
-            { 
-                
+            {
+                if (!this.CheckIndex(index))
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                int[] bits = this.GetBits();
+                return bits[63 - index];
             }
+        }
+
+        public static bool operator ==(BitArray64 arrayOne, BitArray64 arrayTwo)
+        {
+            return arrayOne.Equals(arrayTwo);
+        }
+
+        public static bool operator !=(BitArray64 arrayOne, BitArray64 arrayTwo)
+        {
+            return !arrayOne.Equals(arrayTwo);
+        }
+
+        public override bool Equals(object obj)
+        {
+            BitArray64 equalTo = obj as BitArray64;
+
+            return this.number.Equals(equalTo.number);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.number.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return String.Join("", this);
+        }
+
+        private bool CheckIndex(int index)
+        {
+            if (index < 0 || index > 63)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        private int[] GetBits()
+        {
+            ulong number = this.number;
+
+            int[] bits = new int[64];
+
+            int counter = 63;
+
+            while (number > 0)
+            {
+                bits[counter] = (int)number % 2;
+                number = number / 2;
+                counter--;
+            }
+
+            do
+            {
+                bits[counter] = 0;
+                counter--;
+            } while (counter >= 0);
+
+            return bits;
         }
     }
 }
--- OOP/06 Common Type System/ProblemBitArray64/MainTest.cs
+++ OOP/06 Common Type System/ProblemBitArray64/MainTest.cs
@@ -10,6 +10,18 @@
     {
         static void Main(string[] args)
         {
+            BitArray64 num1 = new BitArray64(1024);
+            BitArray64 num2 = new BitArray64(byte.MaxValue);
+
+            Console.WriteLine("Num1: {0}", num1);
+            Console.WriteLine("Num2: {0}", num2);
+
+            Console.WriteLine("Num1 == Num2 ==> {0}", num1 == num2);
+
+            Console.WriteLine("Num1[ 9] ==> {0}", num1[9]);
+            Console.WriteLine("Num1[10] ==> {0}", num1[10]);
+            Console.WriteLine("Num1[11] ==> {0}", num1[11]);
+
         }
     }
 }
