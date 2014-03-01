b1820ac663792f6b335d08c62f2c868984237e9b
Tancho Mihov <tancho.mihov@gmail.com>
2014-03-01 23:07:06 +0200

    move .sln

---
+++ OOP/06 Common Type System/ProblemBitArray64/BitArray64.cs
@@ -0,0 +1,28 @@
+﻿namespace ProblemBitArray64
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class BitArray64 : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int this[int index]
+        {
+            get
+            { 
+                
+            }
+        }
+    }
+}
